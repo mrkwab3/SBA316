ec94f404e20699ff09a54ba3996fcaca7bdbb990
mrkwab3 <mrkwab3@gmail.com>
2025-02-13 19:04:27 -0500

    Html boilerplate and main queries completed

---
+++ index.js
@@ -0,0 +1,23 @@
+const header = document.getElementById('dom')
+const synopsis = document.querySelector('div.synopsis')
+const mainInfo = document.querySelector('div.mainInfo')
+const conclusion = document.querySelector('div.conclusion')
+
+// * Header
+
+header.innerHTML()
+
+// * Synopsis
+
+// * Main Info
+
+// * Conclusive Info
+
+
+const p = document.createElement('p')
+p.textContent = 'lorem ipsum dolor'
+
+synopsis.appendChild(p)
+
+
+
