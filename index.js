97ee94eab0607deff1e48d5e63232fdb876881c6
mrkwab3 <mrkwab3@gmail.com>
2025-02-14 17:27:34 -0500

    Removed radio buttons for bloat

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
     * - Modify at least one attribute of an element in response to user interaction.
     * - Register at least two different event listeners and create the associated event handler functions.
     * - Use at least two Browser Object Model (BOM) properties or methods. (Completed)
-    TODO - Include at least one form and/or input with HTML attribute validation.
+    * - Include at least one form and/or input with HTML attribute validation.
     TODO - Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
 */
 
@@ -90,6 +90,7 @@ labelPassword.style = 'margin-left: 5px'
 
 const password = document.createElement('input')
 password.type = 'password'
+password.minLength = 6
 form.appendChild(password)
 
 // radio checklist
@@ -132,9 +133,9 @@ img.addEventListener('click', changeImg)
 mainInfo.prepend(img)
 
 // * Conclusive Info
-// let conclusionTitle = window.prompt('Please input a title for the conclusion card')
-// let conclusionContent = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
-// conclusion.appendChild(createCard(conclusionTitle, conclusionContent))
+let conclusionTitle = window.prompt('Please input a title for the conclusion card')
+let conclusionContent = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+conclusion.appendChild(createCard(conclusionTitle, conclusionContent))
 
 
 
